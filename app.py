513bca227da4f8f61f23d8479738cb8b8bfaa964
Manuela Borges <manubramos13@gmail.com>
2024-10-11 15:20:10 -0300

    aula 11/10

--- app.py
+++ app.py
@@ -4,19 +4,30 @@ app = Flask(__name__)
 
 @app.route("/", methods=('GET',))
 def index():
-    nome = request.args.get('nome')
-    return f"""<h1>Página Inicial</h1> 
-    <P>Olá {nome}, que nome bonito!</P>
-    """
+    return "<h1>Página Inicial</h1>"
 
-@app.route("/galeria", methods =('GET',))
-def galeria():
-    return "<h1>Galeria</h1>"
+@app.route("/area")
+def area():
+  altura = float (request.args.get('a'))
+  largura = float(request.args.get('l'))
+  return f""" 
+<h1> A área informada> L={largura}* A={altura} Area={largura*altura}</h1>"""
 
-@app.route("/contato", methods =('GET',))
-def contato():
-    return "<h1>Contato</h1>"
+@app.route("/parimpar", methods=('GET',))
+def parimpar():
+  numero = float(request.args.get('n'))
+  if numero % 2 == 0:
+    return f"O número {numero} é par."
+  else:
+    return f"O número {numero} é ímpar."
+  
+@app.route("/nomesob", methods=('GET',))
+def nomesob():
+  nome = request.args.get('nome')
+  sobrenome = request.args.get('sobrenome')
+  return f"""<h1> Sobrenome </h1>
+  <p>{sobrenome},{nome}</p>"""
 
-@app.route("/Sobre", methods =('GET',))
-def sobre():
-    return "<h1>Sobre</h1>"
+
+
+
