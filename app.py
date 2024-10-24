de54c12b14fca7adfe6437be9f9a31058740f7b9
Manuela Borges <manubramos13@gmail.com>
2024-10-24 15:22:56 -0300

    aula 24-20

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import (Flask, request)
+from flask import (Flask, render_template, request) 
 
 app = Flask(__name__)
 
-@app.route("/sting:nome", methods=('GET',))
+@app.route("/", methods=('GET',))
 def index():
-    return "<h1>Página Inicial</h1>"
+    nome = request.args.get('nome')
+    return f"""<h1>Página inicial</h1> <p>Eu sou a Manu</p>
+        <p>Olá {nome}, que nome bonito!
+    """
+@app.route("/galeria", methods=('GET',))
+def galeria():
+    return "<h1>Galeria</h1>"
 
-@app.route("/area/<float:altura>/<float:largura>" , methods=("GET",))
-def area(largura, altura):
-    
-    return f"""<h1> A área informada> L={largura}* A={altura} => Area={largura*altura} <h1>"""
+@app.route("/contato", methods=('GET',))
+def contato():
+    return "<h1>Contato (17) 99111-2323</h1>"
 
-@app.route("/parimpar/<float:numero>", methods=('GET',))
+@app.route("/sobre", methods=('GET',))
+def sobre():
+    return "<h1>Sobre...</h1>"
+
+@app.route("/area")
+def area():
+  altura = float (request.args.get('a'))
+  largura = float(request.args.get('l'))
+  return f""" 
+<h1> A área informada> L={largura}* A={altura} Area={largura*altura}</h1>"""
+
+@app.route("/parimpar", methods=('GET',))
 def parimpar():
   numero = float(request.args.get('n'))
   if numero % 2 == 0:
@@ -26,9 +42,12 @@ def nomesob():
   return f"""<h1> Sobrenome </h1>
   <p>{sobrenome},{nome}</p>"""
 
-@app.route("/potencia/<float:numero>/<float:elevado>", methods=('GET',))
-def potencia(numero: float, elevado: float):
-    return f"""<h1>A potencia é> N={numero}* E={elevado} => Potencia={numero*elevado}</h1>"""
-
+@app.route("/tabuada")
+@app.route("/tabuada/<numero>", methods=("GET", ))
+def tabuada(numero=None): #None desobriga o valor
+  if 'numero' in request.args:
+     numero= int(request.args.get('numero'))
+  
+  return render_template('tabuada.html',numero=numero)
 
 
